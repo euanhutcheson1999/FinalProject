5e8c36a546b8c121cee7a6784f43b08d2b8e0ddc
Wootten <harrisonwootten@hotmail.com>
2021-10-20 11:19:05 +0100

    Attempt to add cucumber test

--- src/test/java/hellocucumber/StepDefinitions.java
+++ src/test/java/hellocucumber/StepDefinitions.java
@@ -1,13 +1,16 @@
 package hellocucumber;
 
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import Jukebox.Song;
+import Jukebox.Playlist;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class StepDefinitions {
+    //Song Tests
     @Given("I have a song to add")
     public void iHaveASongToAdd() {
     }
@@ -21,8 +24,35 @@ public class StepDefinitions {
         return song.getSongTitle();
     }
 
-    @Then("{string} should be created")
-    public void songShouldBeCreated(String newSong) {
-        assertEquals("Thong Song", newSong, "Song was not correct");
+    @Then("Song should be created")
+    public void songShouldBeCreated() {
+        String ourSong = iEnterTheSongDetails();
+        assertEquals("Thong Song", ourSong, "Song was not correct");
+    }
+
+    //Playlist Tests
+    @Given("I have a song to add to a playlist")
+    public Song iHaveASongToAddToAPlaylist() {
+        Song song = new Song("In Too Deep", "Sum41", "Punk-Rock");
+        return song;
+    }
+
+    @And("I have a playlist")
+    public Playlist iHaveAPlaylist() {
+        Playlist myPlaylist = new Playlist();
+        return myPlaylist;
+    }
+
+    @When("I enter the song into the playlist")
+    public void iEnterTheSongIntoThePlaylist() {
+
+    }
+
+    @Then("Song should be added to playlist")
+    public void songShouldBeAddedToPlaylist() {
+    }
+
+    @And("playlist is displayed")
+    public void playlistIsDisplayed() {
     }
 }
