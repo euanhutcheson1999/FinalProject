e3ffb3e30baa5a9bd91e6951457403a614882cba
euanhutcheson1999 <euanhutcheson@Euans-MacBook-Pro.local>
2021-10-20 16:56:23 +0100

    springbootfilestucture

---
+++ src/main/java/Jukebox/springboot.java
@@ -0,0 +1,13 @@
+package Jukebox;
+import com.google.gson.Gson;
+import org.springframework.boot.SpringApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
+//import org.springframework.web.bind.annotation.RequestParam;
+@org.springframework.boot.autoconfigure.SpringBootApplication
+@RestController
+public class springboot {
+}
